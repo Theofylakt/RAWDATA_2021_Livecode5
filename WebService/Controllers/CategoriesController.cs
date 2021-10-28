aceeb6280d773d97927776af25e6efc249396b66
Henrik Bulskov <bulskov@ruc.dk>
2021-10-28 11:04:22 +0200

    Add controller

---
+++ WebService/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : Controller
+    {
+               
+        public string DoSomething()
+        {
+            return "Hello from controller";
+        }
+    }
+}
